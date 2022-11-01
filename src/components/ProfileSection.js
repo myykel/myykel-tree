c670aa74726304b5e21a4396aca9277f34aa8840
Michael Eboji <michael.eboji@ubagroup.com>
2022-11-01 11:54:59 +0100

    Fixed Alt code

--- src/components/ProfileSection.js
+++ src/components/ProfileSection.js
@@ -7,7 +7,7 @@ import Myykel from "../assets/myykel.jpg";
 export default function ProfileSection() {
   return (
     <div id='profile_section'>
-      <img id='profile__img' src={Myykel}/>
+      <img id='profile__img'alt='profile' src={Myykel}/>
       <h4 id='twitter' >Michael Eboji</h4>
       {false && <p id='slack'>Michael Eboji</p>}
     </div>
